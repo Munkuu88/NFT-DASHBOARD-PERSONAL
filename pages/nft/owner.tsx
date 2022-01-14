cfb5bc02b41fb24df25c866a301b608eda73492e
cybinon <nikorunikk@gmail.com>
2022-01-14 18:12:11 +0800

    dashboard

--- pages/nft/owner.tsx
+++ pages/nft/owner.tsx
@@ -2,13 +2,43 @@ import type { NextPage } from "next";
 import { Home } from "../../domain/home";
 import { useAxios } from "../../axios";
 import { Owners } from "../../domain/nft/owner";
+import { Box, Button } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomePage: NextPage = () => {
-  let datas = []
-  datas = useAxios("https://api.nft.mn/nft1003/analytic/info/nft/owner");
+  let [datas, setData] = useState([])
+  let collections = []
+  let [id, setId] = useState('61d4f030df4a9394c8aa237c')
 
-  console.log(datas);
-  return <Owners datas={datas}  />;
+  const clickHanle = (e: any) => {
+    setId(e.target.value)
+  }
+
+  useEffect(() => {
+    setData([])
+    axios.get("https://nft-analytic-system.herokuapp.com/nft/owners/" + id).then(res => {
+      setData(res.data)
+    })
+  }, [id])
+
+  collections = useAxios("https://api.nft.mn/nft1003/v1/nft/collection");
+
+  return (
+    <>
+      <Box>
+        {collections.map((collection: any) => {
+          return (
+            <Button value={collection._id} onClick={clickHanle} margin={3}>
+              {collection.name}
+            </Button>
+          )
+        })
+        }
+      </Box>
+      <Owners datas={datas} />;
+    </>
+  )
 };
 
 export default HomePage;
