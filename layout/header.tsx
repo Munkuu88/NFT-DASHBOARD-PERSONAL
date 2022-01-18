540eed6779588eb500cc1ab06b13bee09fa2d010
cybinon <nikorunikk@gmail.com>
2022-01-18 12:05:21 +0800

    header

--- layout/header.tsx
+++ layout/header.tsx
@@ -4,11 +4,6 @@ import Link from "next/link";
 export function Header() {
   return (
     <HStack spacing={"10px"} p="10px">
-      <Link href="/">
-        <Text border="1px solid black" p="10px" cursor="pointer">
-          Зарагдсан
-        </Text>
-      </Link>
       <Link href="/users">
         <Text border="1px solid black" p="10px" cursor="pointer">
           Хэрэлэгчид
